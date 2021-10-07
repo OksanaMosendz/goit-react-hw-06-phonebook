a8eab87438a76f5a7668ef1534bdaab8be11b7ce
OksanaMosendz <bagheera03@gmail.com>
2021-10-07 12:45:07 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,12 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-export const App = () => {
-  // useEffect(() => {
-  //   const parseLocalContacts = JSON.parse(localStorage.getItem('contacts'));
-  //   if (parseLocalContacts) {
-  //     setContacts(parseLocalContacts);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  return (
-    <>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
-    </>
-  );
-};
+export const App = () => (
+  <>
+    <h1>Phonebook</h1>
+    <ContactForm />
+    <h2>Contacts</h2>
+    <Filter />
+    <ContactList />
+  </>
+);
